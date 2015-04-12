ff21a04f35ea79ba9092022ff130653fbeb198a9
Marcello Seri <marcello.seri@gmail.com>
2015-04-12 11:44:26 +0100

    GNC stubbed

--- controllers/gnc.go
+++ controllers/gnc.go
@@ -1,27 +1,37 @@
 package controllers
 
+import (
+  "github.com/willitlaunch/willitlaunch-go/games"
+)
+
 type GNCController struct {
 	FlightControllerBase
 }
 
 func (c *GNCController) Init() {
-/*  c.FlightControllerBase.Init()
-  c.Name = "Surgeon"
-  HRGame1 := games.HeartRateGame{GameBase: games.GameBase{Gid: 0}}
-  HRGame1.Init()
-  HRGame2 := games.HeartRateGame{GameBase: games.GameBase{Gid: 1}}
-  HRGame2.Init()
-  CryoGame1 := games.CryogenicLevelsGame{GameBase: games.GameBase{Gid: 2}}
+  c.FlightControllerBase.Init()
+  c.Name = "GNC"
+
+  CPGame1 := games.CabinPressureGame{GameBase: games.GameBase{Gid: 0}}
+  CPGame1.Init()
+  CPGame1.CLDial.Label = "Fuel Pressurisation Level"
+  CPGame1.SetObjectives([]string{"Fuel pressurisation level must stay stable and in a mid range"})
+  CPGame1.CLResetButton.Label = "Depressurise"
+
+  CryoGame1 := games.CryogenicLevelsGame{GameBase: games.GameBase{Gid: 1}}
   CryoGame1.Init()
-  CryoGame1.CLDial.Label = "Fluid Level"
-  CryoGame1.CLBool.Label = "Headache Detected"
-  CryoGame2 := games.CryogenicLevelsGame{GameBase: games.GameBase{Gid: 3}}
+  CryoGame1.CLDial.Label = "Fuel Temperature"
+  CryoGame1.CLBool.Label = "Fuel Temperature Warning"
+  CryoGame1.SetObjectives([]string{"Keep fuel temperature stable and in the appropriate range"})
+
+  CryoGame2 := games.CryogenicLevelsGame{GameBase: games.GameBase{Gid: 2}}
   CryoGame2.Init()
-  CryoGame2.CLDial.Label = "BAC"
-  CryoGame2.CLBool.Label = "Vital Signs Present"
+  CryoGame2.CLDial.Label = "Fuel Density"
+  CryoGame2.CLBool.Label = "Fuel Density Warning"
+  CryoGame2.SetObjectives([]string{"Keep fuel density stable and in the appropriate range"})
   CryoGame2.SetObjectives([]string{})
-  c.Games = append(c.Games, &HRGame1)
+
+  c.Games = append(c.Games, &CPGame1)
   c.Games = append(c.Games, &CryoGame1)
-  c.Games = append(c.Games, &HRGame2)
-  c.Games = append(c.Games, &CryoGame2) */
+  c.Games = append(c.Games, &CryoGame2)
 }
