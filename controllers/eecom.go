b6d28fa7cedf1d0924d18e342b9ef688e90e061c
Marcello Seri <marcello.seri@gmail.com>
2015-04-12 02:51:06 +0100

    refactored to Base

--- controllers/eecom.go
+++ controllers/eecom.go
@@ -1,25 +1,25 @@
 package controllers
 
 import (
-  "github.com/willitlaunch/willitlaunch-go/games"
+	"github.com/willitlaunch/willitlaunch-go/games"
 )
 
 type EECOMController struct {
-  FlightControllerImpl
+	FlightControllerBase
 }
 
 func (c *EECOMController) Init() {
-  c.Name = "Electrical, Environmental and Consumables Manager (EECOM)"
+	c.Name = "Electrical, Environmental and Consumables Manager (EECOM)"
 
-  CLGame := games.CryogenicLevelsGame{GameImpl: games.GameImpl{Gid: 0}}
-  CLGame.Init()
-  c.Games = append(c.Games, &CLGame)
+	CLGame := games.CryogenicLevelsGame{GameBase: games.GameBase{Gid: 0}}
+	CLGame.Init()
+	c.Games = append(c.Games, &CLGame)
 
-  // CPGame := games.CabinPressureGame{Gid: 1}
-  // CPGame.Init()
-  // c.Games = append(c.Games, &CPGame)
+	// CPGame := games.CabinPressureGame{Gid: 1}
+	// CPGame.Init()
+	// c.Games = append(c.Games, &CPGame)
 
-  // BPGame := games.BatteryPowerGame{Gid: 2}
-  // BPGame.Init()
-  // c.Games = append(c.Games, &BPGame)
+	// BPGame := games.BatteryPowerGame{Gid: 2}
+	// BPGame.Init()
+	// c.Games = append(c.Games, &BPGame)
 }
